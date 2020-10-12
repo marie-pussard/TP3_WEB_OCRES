7cc8e45b2da4eb2952b65decf7fd02f54c73db65
marie <marie.pussard@edu.ece.fr>
2020-10-12 22:41:15 +0200

    question 3 solved

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -15,17 +15,18 @@ class API_WEATHER{
       city = "paris";
     }
     this.city = city;
-    console.log(city);
   }
 
   // Faire la requete à l'API openweathermap
   // Retourne une promise
   fetchTodayForecast(){
+    console.log(this.city);
     return axios
     .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
       crossdomain: true
     })
   }
+  
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
 // Fonction appelée lors du click du bouton
 function start() {
   // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+  let ville = document.getElementById('city-input').value;
+
+  // Si la ville n'est pas définit alors la ville par défault est Paris
+  if(ville.length == 0){ //triple égal pour vérifier égalité entre valeur et type 
+    ville = "paris";
+  }
+  
+
+  const apiWeather = new API_WEATHER(ville);
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
@@ -12,20 +20,19 @@ function start() {
       const data = response.data;
 
       // On récupère l'information principal
-      const ville = document.getElementById('city-input').value;
+      
       const main = data.weather[0].main;
       const description = data.weather[0].description;
       const temp = data.main.temp;
       const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
 
       // Modifier le DOM
-      API_WEATHER.city = ville;
+      
       document.getElementById('today-forecast-main').innerHTML = main;
       document.getElementById('today-forecast-more-info').innerHTML = description;
       document.getElementById('icon-weather-container').innerHTML = icon;
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
 
-      console.log(API_WEATHER.city);
     })
     .catch(function(error) {
       // Affiche une erreur
